e676f8e2aaee6f364e9d69cc420f917d559526cd
Tom Consumes from Nutrition Impossible <146956075+x3emmi@users.noreply.github.com>
2024-09-19 12:38:45 -0400

    the challenge has bee COMPLETED (1) woaah

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3 (12, 4, 4);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = plane.transform.position + new Vector3(24, 12, 2);
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float horizontalInput;
+    public GameObject propeller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * horizontalInput);
+    }
+}
